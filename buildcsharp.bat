f2979183587afe3896ecde94339085a39abacc40
kauailabs <scott@kauailabs.com>
2015-06-16 16:16:06 -1000

    Added new setup and build files
    A new Windows Installer is built using Innosetup.

---
+++ buildcsharp.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+pushd c#
+msbuild /p:Configuration=Release;Platform=x86 navXTools.sln
+popd
---
+++ buildsetup.bat
@@ -0,0 +1,3 @@
+pushd setup
+"C:\Program Files (x86)\Inno Setup 5\Compil32" /cc navx-mxp-setup.iss
+popd
