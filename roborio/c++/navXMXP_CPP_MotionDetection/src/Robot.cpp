af7b7502db2bcf12fcfdce551ec352733bbe355f
kauailabs <scott@kauailabs.com>
2015-08-12 17:27:40 -1000

    Added C++ and Java MotionDetection Examples

---
+++ roborio/c++/navXMXP_CPP_MotionDetection/src/Robot.cpp
@@ -0,0 +1,88 @@
+#include "WPILib.h"
+#include "AHRS.h"
+
+/**
+ * This is a demo program showing the use of the navX MXP to implement
+ * a motion detection feature, which can be used to detect when your
+ * robot is still and when it is not.
+ *
+ * The basic principle used within the Motion Detection example
+ * is to subtract the acceleration due to gravity from the raw
+ * acceleration values. The result value is known as “world linear
+ * acceleration”, representing the actual amount of acceleration
+ * due to motion, and is calculated automatically by navX MXP’s
+ * motion processor.  Whenever the sum of the world linear
+ * acceleration in both the X and Y axes exceeds a “motion
+ * threshold”, motion is occurring.  The navX MXP makes it easy,
+ * simply invoke the "isMoving()" function to determine if motion
+ * is currently underway.
+ */
+
+class Robot: public SampleRobot
+{
+
+    // Channels for the wheels
+    const static int frontLeftChannel	= 2;
+    const static int rearLeftChannel	= 3;
+    const static int frontRightChannel	= 1;
+    const static int rearRightChannel	= 0;
+
+    const static int joystickChannel	= 0;
+
+    RobotDrive robotDrive;	// Robot drive system
+    Joystick stick;			// Driver Joystick
+    AHRS *ahrs;             // navX MXP
+
+public:
+    Robot() :
+            robotDrive(frontLeftChannel, rearLeftChannel,
+                       frontRightChannel, rearRightChannel),	// initialize variables in
+            stick(joystickChannel)								// same order declared above
+    {
+        robotDrive.SetExpiration(0.1);
+        robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert left side motors
+        robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// change to match your robot
+        try {
+            /* Communicate w/navX MXP via the MXP SPI Bus.                                       */
+            /* Alternatively:  I2C::Port::kMXP, SerialPort::Port::kMXP or SerialPort::Port::kUSB */
+            /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details.   */
+            ahrs = new AHRS(SPI::Port::kMXP);
+        } catch (std::exception ex ) {
+            std::string err_string = "Error instantiating navX MXP:  ";
+            err_string += ex.what();
+            DriverStation::ReportError(err_string.c_str());
+        }
+        if ( ahrs ) {
+            LiveWindow::GetInstance()->AddSensor("IMU", "Gyro", ahrs);
+        }
+	}
+
+    /**
+     * Runs the motors with Mecanum drive.
+     */
+    void OperatorControl()
+    {
+        robotDrive.SetSafetyEnabled(false);
+        while (IsOperatorControl() && IsEnabled())
+        {
+            bool motionDetected = ahrs->IsMoving();
+            SmartDashboard::PutBoolean("MotionDetected", motionDetected);
+
+            try {
+                /* Use the joystick X axis for lateral movement,            */
+                /* Y axis for forward movement, and Z axis for rotation.    */
+                /* Use navX MXP yaw angle to define Field-centric transform */
+                robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(),
+                                                  stick.GetZ(),ahrs->GetAngle());
+            } catch (std::exception ex ) {
+                std::string err_string = "Error communicating with Drive System:  ";
+                err_string += ex.what();
+                DriverStation::ReportError(err_string.c_str());
+            }
+            Wait(0.005); // wait 5ms to avoid hogging CPU cycles
+        }
+    }
+
+};
+
+START_ROBOT_CLASS(Robot);
---
+++ roborio/java/navXMXP_Java_MotionDetection/src/org/usfirst/frc/team2465/robot/Robot.java
@@ -0,0 +1,85 @@
+package org.usfirst.frc.team2465.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * This is a demo program showing the use of the navX MXP to implement
+ * a motion detection feature, which can be used to detect when your
+ * robot is still and when it is not.
+ *
+ * The basic principle used within the Motion Detection example 
+ * is to subtract the acceleration due to gravity from the raw 
+ * acceleration values. The result value is known as “world linear 
+ * acceleration”, representing the actual amount of acceleration 
+ * due to motion, and is calculated automatically by navX MXP’s 
+ * motion processor.  Whenever the sum of the world linear 
+ * acceleration in both the X and Y axes exceeds a “motion 
+ * threshold”, motion is occurring.  The navX MXP makes it easy, 
+ * simply invoke the "isMoving()" function to determine if motion
+ * is currently underway.
+ */
+
+public class Robot extends SampleRobot {
+  AHRS ahrs;
+  RobotDrive myRobot;
+  Joystick stick;
+
+  public Robot() {
+      myRobot = new RobotDrive(0, 1);
+      myRobot.setExpiration(0.1);
+      stick = new Joystick(0);
+      try {
+          /* Communicate w/navX MXP via the MXP SPI Bus.                                     */
+          /* Alternatively:  I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB     */
+          /* See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for details. */
+          ahrs = new AHRS(SPI.Port.kMXP); 
+      } catch (RuntimeException ex ) {
+          DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+      }
+  }
+
+  /**
+   * Drive left & right motors for 2 seconds then stop
+   */
+  public void autonomous() {
+      myRobot.setSafetyEnabled(false);
+      myRobot.drive(-0.5, 0.0);	// drive forwards half speed
+      Timer.delay(2.0);		//    for 2 seconds
+      myRobot.drive(0.0, 0.0);	// stop robot
+  }
+
+  /**
+   * Runs the motors with arcade steering.
+   */
+
+  public void operatorControl() {
+      myRobot.setSafetyEnabled(true);
+      while (isOperatorControl() && isEnabled()) {
+
+          boolean motionDetected = ahrs.isMoving();
+          SmartDashboard.putBoolean("MotionDetected", motionDetected);
+          
+          try {
+              myRobot.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getTwist(),0);
+          } catch( RuntimeException ex ) {
+              String err_string = "Drive system error:  " + ex.getMessage();
+              DriverStation.reportError(err_string, true);
+          }
+          Timer.delay(0.005);		// wait for a motor update time
+      }
+  }
+
+  /**
+   * Runs during test mode
+   */
+  public void test() {
+  }
+}
