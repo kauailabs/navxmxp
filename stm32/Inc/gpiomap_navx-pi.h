e991dc358244809f7e31e97236821183c6cc0c53
Kauai Labs <scott@kauailabs.com>
2017-06-25 12:12:31 -1000

    Added some comments regarding GPIO map usage and GPIO interrupts.

--- stm32/Inc/gpiomap_navx-pi.h
+++ stm32/Inc/gpiomap_navx-pi.h
@@ -71,26 +71,26 @@
 #define QE3_IDX_Pin GPIO_PIN_5
 #define QE3_IDX_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
-#define PWM_GPIO1_Pin GPIO_PIN_2 //PA2, TIM5 CH3
+#define PWM_GPIO1_Pin GPIO_PIN_2 //PA2, TIM5 CH3 (PA0?)
 #define PWM_GPIO1_GPIO_Port GPIOA
-#define PWM_GPIO2_Pin GPIO_PIN_3 //PA3, TIM5 CH4
+#define PWM_GPIO2_Pin GPIO_PIN_3 //PA3, TIM5 CH4 (PA1?)* [sh GPIO ch w/TIM2/Ch2]
 #define PWM_GPIO2_GPIO_Port GPIOA
 #define PWM_GPIO3_Pin GPIO_PIN_5 //PE5, TIM9 CH1
 #define PWM_GPIO3_GPIO_Port GPIOE
-#define PWM_GPIO4_Pin GPIO_PIN_6 //PE6, TIM9 CH2
+#define PWM_GPIO4_Pin GPIO_PIN_6 //PE6, TIM9 CH2* [sh GPIO Ch w/TIM3/Ch1]
 #define PWM_GPIO4_GPIO_Port GPIOE
 
 #define QE1_A_Pin GPIO_PIN_9  //PE9, TIM1 CH1
 #define QE1_A_GPIO_Port GPIOE
-#define QE1_B_Pin GPIO_PIN_11 //PE11, TIM1 CH1
+#define QE1_B_Pin GPIO_PIN_11 //PE11, TIM1 CH1 *NVIC:  Already Used [CAL BTN]
 #define QE1_B_GPIO_Port GPIOE
 #define QE2_A_Pin GPIO_PIN_15 //PA15, TIM2 CH1
 #define QE2_A_GPIO_Port GPIOA
-#define QE2_B_Pin GPIO_PIN_3  //PB3, TIM2 CH2
+#define QE2_B_Pin GPIO_PIN_3  //PB3, TIM2 CH2 *NVIC:  Already Used (TIM1/Ch4)
 #define QE2_B_GPIO_Port GPIOB
-#define QE3_A_Pin GPIO_PIN_6  //PC6, TIM3 CH1
+#define QE3_A_Pin GPIO_PIN_6  //PC6, TIM3 CH1 *NVIC:  Already Used (TIM9/Ch2)
 #define QE3_A_GPIO_Port GPIOC
-#define QE3_B_Pin GPIO_PIN_7  //PC7, TIM3 CH2
+#define QE3_B_Pin GPIO_PIN_7  //PC7, TIM3 CH2 *NVIC:  Already Used [CAN]
 #define QE3_B_GPIO_Port GPIOC
 #define QE4_A_Pin GPIO_PIN_12 //PD12, TIM4 CH1
 #define QE4_A_GPIO_Port GPIOD
@@ -98,6 +98,9 @@
 #define QE4_B_GPIO_Port GPIOD
 
 /* USER CODE END Private defines */
+
+#define GPIO_INT(GPIO_INT_NUM) (1 << GPIO_INT_NUM)
+
 
 /**
   * @}
