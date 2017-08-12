e0bc84ae7778358c3ac5b0450f0622518a434f2f
Kauai Labs <scott@kauailabs.com>
2017-08-12 05:54:38 -1000

    Update for V4 of VMX-pi hardware

--- stm32/Inc/gpiomap_navx-pi.h
+++ stm32/Inc/gpiomap_navx-pi.h
@@ -46,8 +46,8 @@
 #define S1_LED_GPIO_Port GPIOC
 #define BOOT1_Pin GPIO_PIN_2
 #define BOOT1_GPIO_Port GPIOB
-#define _CAN_INT_Pin GPIO_PIN_7
-#define _CAN_INT_GPIO_Port GPIOE
+#define _CAN_INT_Pin GPIO_PIN_4
+#define _CAN_INT_GPIO_Port GPIOB
 #define _I2C_DEV_ON_Pin GPIO_PIN_12
 #define _I2C_DEV_ON_GPIO_Port GPIOE
 #define CAN_CS_Pin GPIO_PIN_12
@@ -56,41 +56,35 @@
 #define MPU9250_INT_GPIO_Port GPIOD
 #define CAL_LED_Pin GPIO_PIN_9
 #define CAL_LED_GPIO_Port GPIOD
-#define CAN_STANDBY_Pin GPIO_PIN_10
+#define CAN_STANDBY_Pin GPIO_PIN_11
 #define CAN_STANDBY_GPIO_Port GPIOD
-#define CAL_BTN_Pin GPIO_PIN_11
+#define CAL_BTN_Pin GPIO_PIN_10
 #define CAL_BTN_GPIO_Port GPIOD
-#define QE4_IDX_Pin GPIO_PIN_8      /* Removed in v5_30 */
-#define QE4_IDX_GPIO_Port GPIOA     /* Removed in v5_30 */
-#define QE2_IDX_Pin GPIO_PIN_10     /* RPI INT 4 in v5_30 */
-#define QE2_IDX_GPIO_Port GPIOA     /* RPI INT 4 in v5_30 */
 #define _CAN_RESET_Pin GPIO_PIN_10
 #define _CAN_RESET_GPIO_Port GPIOC
-#define QE1_IDX_Pin GPIO_PIN_11     /* EEPROM WP in v5_30 */
-#define QE1_IDX_GPIO_Port GPIOC     /* EEPROM WP in v5_30 */
-#define QE3_IDX_Pin GPIO_PIN_5      /* Removed in v5_30 */
-#define QE3_IDX_GPIO_Port GPIOD     /* Removed in v5_30 */
 /* USER CODE BEGIN Private defines */
-#define PWM_GPIO1_Pin GPIO_PIN_2 //PA2, TIM5 CH3 (PA0?)
+#define PWM_GPIO1_Pin GPIO_PIN_2 //PA2, TIM5 CH3
 #define PWM_GPIO1_GPIO_Port GPIOA
-#define PWM_GPIO2_Pin GPIO_PIN_3 //PA3, TIM5 CH4 (PA1?)* [sh GPIO ch w/TIM2/Ch2]
+#define PWM_GPIO2_Pin GPIO_PIN_3 //PA3, TIM5 CH4  [sh GPIO ch w/TIM2/Ch2]
 #define PWM_GPIO2_GPIO_Port GPIOA
 #define PWM_GPIO3_Pin GPIO_PIN_5 //PE5, TIM9 CH1
 #define PWM_GPIO3_GPIO_Port GPIOE
-#define PWM_GPIO4_Pin GPIO_PIN_6 //PE6, TIM9 CH2* [sh GPIO Ch w/TIM3/Ch1]
+#define PWM_GPIO4_Pin GPIO_PIN_6 //PE6, TIM9 CH2
 #define PWM_GPIO4_GPIO_Port GPIOE
 
 #define QE1_A_Pin GPIO_PIN_9  //PE9, TIM1 CH1
 #define QE1_A_GPIO_Port GPIOE
-#define QE1_B_Pin GPIO_PIN_11 //PE11, TIM1 CH1 *NVIC:  Already Used [CAL BTN]
+#define QE1_B_Pin GPIO_PIN_11 //PE11, TIM1 CH1
 #define QE1_B_GPIO_Port GPIOE
 #define QE2_A_Pin GPIO_PIN_15 //PA15, TIM2 CH1
 #define QE2_A_GPIO_Port GPIOA
-#define QE2_B_Pin GPIO_PIN_3  //PB3, TIM2 CH2 *NVIC:  Already Used (TIM1/Ch4)
+#define QE2_B_Pin GPIO_PIN_3  //PB3, TIM2 CH2 [sh GPIO ch w/ TIM5/Ch4]
 #define QE2_B_GPIO_Port GPIOB
-#define QE3_A_Pin GPIO_PIN_6  //PC6, TIM3 CH1 *NVIC:  Already Used (TIM9/Ch2)
+#define QE3_A_Pin GPIO_PIN_6  //PC6, TIM3 CH1
 #define QE3_A_GPIO_Port GPIOC
-#define QE3_B_Pin GPIO_PIN_7  //PC7, TIM3 CH2 *NVIC:  Already Used [CAN]
+#define QE3_A_Second_Pin GPIO_PIN_2   /* Used for interrupt input */
+#define QE3_A_Second_GPIO_Port GPIOD  /* Used for interrupt input */
+#define QE3_B_Pin GPIO_PIN_7  //PC7, TIM3 CH2
 #define QE3_B_GPIO_Port GPIOC
 #define QE4_A_Pin GPIO_PIN_12 //PD12, TIM4 CH1
 #define QE4_A_GPIO_Port GPIOD
@@ -128,7 +122,7 @@
 #define RPI_GPIO_DIR_IN_GPIO_Port GPIOB
 
 /* CAN Interrupt Signal */
-#define NAVX_2_RPI_INT3_Pin GPIO_PIN_2
+#define NAVX_2_RPI_INT3_Pin GPIO_PIN_1
 #define NAVX_2_RPI_INT3_GPIO_Port GPIOD
 
 #define _IO_POWER_FAULT_Pin GPIO_PIN_4
