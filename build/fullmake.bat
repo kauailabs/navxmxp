28f9eec894e73f3cf4d44b207ff7b412d694a24b
Scott Libert <scott@kauailabs.com>
2019-01-04 11:45:41 -1000

    Built against WPI Library 2019.1.1 Kickoff Release.

--- build/fullmake.bat
+++ build/fullmake.bat
@@ -4,6 +4,6 @@ call dosyncftp_docs
 call buildall
 call makedist
 call makedist_navX-Micro
-call dosyncftp
+rem call dosyncftp
 rem call dosyncftp_navX-Micro
-call maven_deploy
+rem call maven_deploy
---
+++ build/readme.txt
@@ -0,0 +1,2 @@
+1/4/2019:  Compiled against latest (2019) release of the WPI Library (2019.1.1 Kickoff Release).
+
