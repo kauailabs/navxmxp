e624390eeb7918ea11aa48fb1cbbfae173190660
Scott Libert <scott@kauailabs.com>
2019-12-10 17:12:28 -1000

    Check in modified fullmake build file.

--- build/fullmake.bat
+++ build/fullmake.bat
@@ -3,7 +3,7 @@ call makedocs
 call dosyncftp_docs
 call buildall
 call makedist
-rem call makedist_navX-Micro
+call makedist_navX-Micro
 call dosyncftp
 rem call dosyncftp_navX-Micro
 call maven_deploy
