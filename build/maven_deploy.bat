7a71d695af1251df6378c99cf11ffefe4849ff8f
Scott Libert <scott@kauailabs.com>
2020-02-04 18:56:21 -1000

    Update maven deploy script to hopefully work around some reliability issues encountered during uploads to Sonatype Nexus (Maven Central) Repository.

--- build/maven_deploy.bat
+++ build/maven_deploy.bat
@@ -1,11 +1,11 @@
 set WINSCP="C:\Program Files (x86)\WinSCP\WinSCP.com"
 %WINSCP% /script=dosyncftp_maven.cmd
 REM Publish artifact to MavenCentral
-pushd ..\roborio\c++\navx_frc_cpp
+pushd ..\roborio\java\navx_frc
 call gradlew publish
 popd
 REM Publish artifact to MavenCentral
-pushd ..\roborio\java\navx_frc
+pushd ..\roborio\c++\navx_frc_cpp
 call gradlew publish
 popd
 
