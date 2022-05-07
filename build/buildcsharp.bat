3f651b5d5963d29eebee3fa5d2306ddd037e533f
Scott Libert <scott@kauailabs.com>
2022-05-06 15:55:22 -1000

    Updated buildcsharp batch file to use VS2019.

--- build/buildcsharp.bat
+++ build/buildcsharp.bat
@@ -1,4 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 pushd ..
 pushd c#
 msbuild /p:Configuration=Release;Platform=x86 navXTools.sln
